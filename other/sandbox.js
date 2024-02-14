a2d0dc523a64422aef4ed62f256d6ab3117865c6
Zoltán Berkula <64430345+zoltanberkula@users.noreply.github.com>
2024-02-14 12:41:24 +0100

    update

--- other/sandbox.js
+++ other/sandbox.js
@@ -1,21 +1,21 @@
 const parser = require("binary-buffer-parser");
 const nio = require("zzz.skein");
-// function readInputRegs(slaveID, addr, len) {
-//   const fnc = 0x03;
-//   createFrame(slaveID, fnc, addr, len);
-//   uart.write(q2.receive(), (err) => {
-//     err && console.log(err);
-//   });
-//   const promise = new Promise((resolve, reject) => {
-//     event.once("dataReady", (data) => {
-//       resolve(data);
-//     });
-//     setTimeout(() => {
-//       reject(new Error("Request timed out"));
-//     }, 2000);
-//   });
-//   return promise;
-// }
+function readInputRegs(slaveID, addr, len) {
+  const fnc = 0x03;
+  createFrame(slaveID, fnc, addr, len);
+  uart.write(q2.receive(), (err) => {
+    err && console.log(err);
+  });
+  const promise = new Promise((resolve, reject) => {
+    event.once("dataReady", (data) => {
+      resolve(data);
+    });
+    setTimeout(() => {
+      reject(new Error("Request timed out"));
+    }, 2000);
+  });
+  return promise;
+}
 
 const sID = 0x01;
 const raddr = 0x01;
@@ -27,17 +27,17 @@ let buf = nio.Skein.wrap([sID, raddr, rlen]);
 console.log(buf.get());
 console.log(buf.get());
 console.log(buf.get());
-//console.log(bufi);
+console.log(bufi);
 
 const data = [0x01, 0x03, 0x00, 0x00, 0x00, 0x01, 0x84, 0x0a];
 const response = [0x01, 0x03, 0x02, 0x01, 0x0a, 0x39, 0xd3];
 const buffer = Buffer.from(response);
-//console.log(buffer);
+console.log(buffer);
 
 const address = [0x00, 0x01];
 const z = [0x00, 0x00];
 const pseudoAddr = [...z, ...address];
-//console.log(pseudoAddr.slice(-2));
+console.log(pseudoAddr.slice(-2));
 
 const acrelObj = {
   DCV: 0,
@@ -45,10 +45,10 @@ const acrelObj = {
   IntT: 5,
 };
 
-// const bufferParser = new parser(buffer);
+const bufferParser = new parser(buffer);
 
-// console.log(bufferParser.int8());
-// console.log(bufferParser.int16());
+console.log(bufferParser.int8());
+console.log(bufferParser.int16());
 
 function bufferParser(buffer) {
   console.log("Incoming buffer:", buffer);
@@ -67,23 +67,23 @@ function bufferParser(buffer) {
   }
 }
 
-//bufferParser(buffer);
+bufferParser(buffer);
 
-//wrapper function implementation
-//looping through a given object
+// wrapper function implementation
+// looping through a given object
 
-// function readAcrelEM(slaveID, obj) {
-//   if (!Object.entries(obj)) {
-//     console.log("Empty Object!");
-//   }
-//   for (let [key, value] of Object.entries(obj)) {
-//     readHoldingRegs(slaveID, value, 1)
-//       .then((data) => bufferParser(data))
-//       .catch((err) => console.log(err));
-//   }
-// }
+function readAcrelEM(slaveID, obj) {
+  if (!Object.entries(obj)) {
+    console.log("Empty Object!");
+  }
+  for (let [key, value] of Object.entries(obj)) {
+    readHoldingRegs(slaveID, value, 1)
+      .then((data) => bufferParser(data))
+      .catch((err) => console.log(err));
+  }
+}
 
-// readAcrelEM(1, acrelObj);
+readAcrelEM(1, acrelObj);
 
 function solve(addr) {
   if (addr < 0x00ff) {
@@ -94,56 +94,56 @@ function solve(addr) {
   }
 }
 
-// solve(0x22ff);
-// solve(0x02);
+solve(0x22ff);
+solve(0x02);
 
-// mqtt.connect();
-// setInterval(() => {
-//   readData2()
-//     .then((storage) => console.log("STR:", storage))
-//     .then(() => {
-//       mqtt.publish(JSON.stringify(createPyld(), null, 2));
-//       console.log(createPyld());
-//     })
-//     .catch((err) => console.log(err));
-// }, 3000);
+mqtt.connect();
+setInterval(() => {
+  readData2()
+    .then((storage) => console.log("STR:", storage))
+    .then(() => {
+      mqtt.publish(JSON.stringify(createPyld(), null, 2));
+      console.log(createPyld());
+    })
+    .catch((err) => console.log(err));
+}, 3000);
 
-// const pollObject = {
-//   read1: acrelReadHoldingReg(acrelMeasurand, 1),
-//   read2: acrelReadHoldingReg(acrelDualMeasurand, 2),
-// };
-// async function read1(num) {
-//   await console.log("PRINT1:", num);
-//   acrelReadHoldingReg(acrelMeasurand, 1);
-//   return;
-// }
+const pollObject = {
+  read1: acrelReadHoldingReg(acrelMeasurand, 1),
+  read2: acrelReadHoldingReg(acrelDualMeasurand, 2),
+};
+async function read1(num) {
+  await console.log("PRINT1:", num);
+  acrelReadHoldingReg(acrelMeasurand, 1);
+  return;
+}
 
-// async function read2(num) {
-//   await console.log("PRINT2:", num);
-//   acrelReadHoldingReg(acrelDualMeasurand, 2);
-//   return;
-// }
+async function read2(num) {
+  await console.log("PRINT2:", num);
+  acrelReadHoldingReg(acrelDualMeasurand, 2);
+  return;
+}
 
-// async function read3(num) {
-//   await console.log("PRINT3:", num);
-//   return;
-// }
-// setInterval(() => {
-//   async.parallel(
-//     [
-//       function (callback) {
-//         callback(null, "1");
-//         read1(1);
-//       },
-//       function (callback) {
-//         callback(null, "2");
-//         read2(2);
-//       },
-//     ],
-//     function (err, results) {
-//       err && console.log(err);
-//       console.log("Results:", results);
-//       console.log(createPyld());
-//     }
-//   );
-// }, 3000);
+async function read3(num) {
+  await console.log("PRINT3:", num);
+  return;
+}
+setInterval(() => {
+  async.parallel(
+    [
+      function (callback) {
+        callback(null, "1");
+        read1(1);
+      },
+      function (callback) {
+        callback(null, "2");
+        read2(2);
+      },
+    ],
+    function (err, results) {
+      err && console.log(err);
+      console.log("Results:", results);
+      console.log(createPyld());
+    }
+  );
+}, 3000);
